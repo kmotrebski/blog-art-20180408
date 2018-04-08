772d88f06d1f9e7b1fc8bb8b6b4d41b91a531899
Konrad Otrębski <11849561+kmotrebski@users.noreply.github.com>
2018-04-08 19:34:12 +0200

    Added smoke/end-2-end test to test the app running in a container

--- build.sh
+++ build.sh
@@ -6,6 +6,11 @@ clear
 CONTAINER_NAME="unit_tests"
 SCOPE="tests/php/Unit"
 
+#colors
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
 source ./.env
 
 docker run --rm --interactive --tty \
@@ -24,4 +29,18 @@ docker run \
         ${DOCKER_REG}/blog-app-1:dev \
         vendor/phpunit/phpunit/phpunit --bootstrap tests/php/Unit/TestBootstrap.php ${SCOPE}
 
-docker curl -XGET localhost:${HTTP_PORT}
+docker run -d \
+        --volume $(pwd):/var/www/html/blog-app \
+        --name run_e2e \
+        -p "${HTTP_PORT}:80" \
+        --rm \
+        ${DOCKER_REG}/blog-app-1:dev
+
+until [ "200" = $(curl --write-out %{http_code} --silent --output /dev/null localhost:${HTTP_PORT}) ]; do
+    printf "${YELLOW}Waiting for container to start up...${NC}\n"
+    sleep 1
+done
+
+docker stop run_e2e
+
+printf "${GREEN}Build finished with success!${NC}\n"
