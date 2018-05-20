870859c09c67675324863584cf9a32602918bf9f
Konrad Otrębski <11849561+kmotrebski@users.noreply.github.com>
2018-05-20 23:18:41 +0200

    Made Docker login to use stdin provided password

--- push.sh
+++ push.sh
@@ -7,7 +7,6 @@ RED='\033[0;31m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
-#todo sprawic ze jak nie ma ustawionego hasła do docker hub to deploy przerywany jest errorem
 #todo DOCKER_REG rename na DOCKER_REG_LOGIN
 
 #set up DOCKER_REG and DOCKER_REG_PASSWORD variable
@@ -15,9 +14,10 @@ source build/setup_push_variables.sh
 
 # push Docker images to the repository
 
-docker version
+#todo remove
+printf "${YELLOW}Login: ${DOCKER_REG}${NC}\n"
 
-eval "docker login --username ${DOCKER_REG} --password ${DOCKER_REG_PASSWORD}"
+echo ${DOCKER_REG_PASSWORD} | docker login -u ${DOCKER_REG} --password-stdin
 
 docker push ${DOCKER_REG}/blog-app-1:dev
 docker push ${DOCKER_REG}/blog-app-1:prod
